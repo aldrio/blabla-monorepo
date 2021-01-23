33586123aa6f4e5a598d5114287e940cc46cd8c8
Brandon Aldrich <brandon@aldr.io>
2021-01-23 02:09:03 -0500

    Add husky

---
+++ husky.config.js
@@ -0,0 +1,9 @@
+const inShell = (cmd) => `nix-shell --pure --run "${cmd}"`;
+const all = (args) => args.join(" && ");
+
+module.exports = {
+  hooks: {
+    "pre-commit": inShell(all(["echo Running pre-commit"])),
+    "post-checkout": inShell(all(["echo Running post-checkout"])),
+  },
+};
