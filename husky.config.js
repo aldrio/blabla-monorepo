19e9276c2388ef23d569b8af3f82779c8ea44e13
Brandon Aldrich <brandon@aldr.io>
2021-01-23 22:14:40 -0500

    Run lints before commit

--- husky.config.js
+++ husky.config.js
@@ -3,7 +3,9 @@ const all = (args) => args.join(' && ')
 
 module.exports = {
   hooks: {
-    'pre-commit': inShell(all(['echo Running pre-commit'])),
+    'pre-commit': inShell(
+      all(['echo Running pre-commit', 'yarn foreach-changed -p run lint'])
+    ),
     'post-checkout': inShell(all(['echo Running post-checkout'])),
   },
 }
